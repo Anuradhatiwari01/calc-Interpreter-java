18eb6f401f6538bece6593ac48b2576f5bd41b4d
Arun Kumar <as9910new@gmail.com>
2026-03-21 22:28:04 +0530

    Initialising the type of Tokens accepted in our CALC interpreter
    This commit is first try to identify which type of tokens are we really accepting in order to develop our CALC-Interpreter. 
    It defines:
    Number
    Identifiers
    Plus
    Minus
    Star(multiply)
    Slash
    caret
    percent
    equals
    Lparen
    Rparen

---
+++ TokenType.java
@@ -0,0 +1,17 @@
+package calc;
+
+public enum TokenType {    
+    NUMBER,       
+    IDENTIFIER,   
+    PLUS,         
+    MINUS,        
+    STAR,         
+    SLASH,        
+    CARET,        
+    PERCENT,     
+    EQUALS,       
+    LPAREN,      
+    RPAREN,      
+
+    EOF    
+}
