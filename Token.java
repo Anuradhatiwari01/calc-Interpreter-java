c317780e2bbc9a68728c28f056a794e863873458
Arun Kumar <as9910new@gmail.com>
2026-03-24 00:45:52 +0530

    Add Token class for token representation

---
+++ Token.java
@@ -0,0 +1,14 @@
+package calc;
+public class Token {
+    public final TokenType type;
+    public final String value;
+
+    public Token(TokenType type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+    @Override
+    public String toString() {
+        return "Token(" + type + ", \"" + value + "\")";
+    }
+}
