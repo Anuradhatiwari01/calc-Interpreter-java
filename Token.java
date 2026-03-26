2ca3840c8049786d3cfe7daeaf17d2efe6a1ceb9
Arun Kumar <as9910new@gmail.com>
2026-03-27 01:11:12 +0530

    Refactor Token class to add line number and validations
    Refactored Token class to include line number and improved null checks.

--- Token.java
+++ Token.java
@@ -1,14 +1,42 @@
-package calc;
-public class Token {
-    public final TokenType type;
-    public final String value;
+import java.util.Objects;
 
-    public Token(TokenType type, String value) {
-        this.type = type;
-        this.value = value;
+public final class Token {
+    private final TokenType type;
+    private final String    value;
+    private final int       line;
+
+    public Token(TokenType type, String value, int line) {
+        this.type  = Objects.requireNonNull(type,  "TokenType must not be null");
+        this.value = Objects.requireNonNull(value, "Token value must not be null");
+        this.line  = line;
+    }
+
+
+    public TokenType getType()  { return type;  }
+    public String    getValue() { return value; }
+    public int       getLine()  { return line;  }
+    public boolean isType(TokenType t) {
+        return this.type == t;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj==null)  return false;
+        if (this == obj) return true;
+        if (!(obj instanceof Token)) return false;
+        Token other = (Token) obj;
+        return type == other.type
+            && value.equals(other.value)
+            && line == other.line;
     }
     @Override
+    public int hashCode() {
+        return Objects.hash(type, value, line);
+    }
+
+    @Override
     public String toString() {
-        return "Token(" + type + ", \"" + value + "\")";
+        return String.format("Token(%-14s %-10s line=%d)",
+            type, "\"" + value + "\"", line);
     }
 }
